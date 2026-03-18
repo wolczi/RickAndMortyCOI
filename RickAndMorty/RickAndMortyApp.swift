da64f36d7eb2e886781bc721c11d06a1d73044e4
Przemyslaw Wolczacki <pwolczacki@9bits.com>
2026-03-18 20:24:17 +0100

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Przemek Wołczacki on 18/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
